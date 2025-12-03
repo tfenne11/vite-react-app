4703c2e8eacf0d88b81cbd5bb074089ab0bfdb0b
Developer <dev@example.com>
2025-12-03 08:14:49 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vite-react-app/',
   plugins: [react()],
   ssr: {
     noExternal: ['@vitejs/plugin-react']
