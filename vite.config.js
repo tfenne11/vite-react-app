63888c70fd7a958a755f9eee8db68d42ca7a79af
Developer <dev@example.com>
2025-12-03 07:37:27 -0600

    Add GitHub Actions workflow to deploy to GitHub Pages; set Vite base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/vite-react-app/',
   plugins: [react()],
   ssr: {
     noExternal: ['@vitejs/plugin-react']
